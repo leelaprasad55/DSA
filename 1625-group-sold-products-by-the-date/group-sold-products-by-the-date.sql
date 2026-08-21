46f28a58ee9e924550862ce59ef6f18d3733c6a4
leelaprasad55 <v.leelaprasad55@gmail.com>
2026-08-21 22:48:06 +0530

    Time: 306 ms (78.63%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -0,0 +1,4 @@
+-- Write your PostgreSQL query statement below
+select sell_date,count(distinct product) as num_sold,string_agg(distinct product, ',' order by product) as products
+from activities
+group by sell_date
